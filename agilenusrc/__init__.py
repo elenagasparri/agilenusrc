f22a1d666c14716aa9f7025af05dcc359e1390bc
Elena Gasparri <elena.gasparri@gmail.com>
2022-03-19 15:22:58 +0100

    minor

---
+++ agilenusrc/__init__.py
@@ -0,0 +1,1 @@
+from agilenusrc.version import __version__
--- agilenusrc/agilenusrc.py
+++ agilenusrc/agilenusrc.py
@@ -1,1 +1,2 @@
-"""First implementation of the program for searching candidate source for IceCube neutrino event IC170922A"""
+"""First implementation of the program for searching candidate source for IceCube neutrino event IC170922A.
+"""
