40eb3c94abcc720b7268f18ff10c147356b5355d
elenagasparri <elena.gasparri@gmail.com>
2022-04-03 16:11:00 +0200

    Update test_dnn.py

--- tests/test_dnn.py
+++ tests/test_dnn.py
@@ -1,4 +1,4 @@
-"""Unit test for the Bidirectional LSTM neural network build in order to classifying blazars.
+"""Unit test for the DataSet.
 """
 
 import unittest
@@ -9,5 +9,28 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 if sys.flags.interactive:
     plt.ion()
+    
+from agilenusrc.BlazarDnn import BlazarDNN
+
+class testBlazarDnn(unittest.testcase):
+    '''Unit test for the BlazarDnn module.
+    '''
+    
+    def test_rescale(self, n=200.):
+        '''Test that rescaleData function returns an
+        nd-array with value in axis=0 between [0,1].
+        '''
+        d=np.linspace(1,100,n)
+        rd = np.reshape(d, (10,10,2))
+        run = BlazarDnn()
+        data_norm = run.rescaleData(rd)
+        
+        self.assertTrue(data_norm[:,:,0].min()==0)
+        self.assertTrue(data_norm[:,:,0].max()==1)
+        
+if __name__ = '__main__':
+    unittest.main(exit=not sys.flags.interactive)
+    
+    
 
 
